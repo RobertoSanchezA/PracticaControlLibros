6502040852f9e443d29b324d19d7c1ca16ea9510
roberto <usr2266@salesianos-lacuesta.net>
2022-02-15 20:33:27 +0000

    Feature: Added button component on main View

--- App.js
+++ App.js
@@ -1,14 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { useState } from 'react';
+import { Button, FlatList, StyleSheet, Text, View } from 'react-native';
+import Navigation from './components/Navigation'
+import Book from './components/Book'
 export default function App() {
+
+  const [bookList, setBooksList] = useState([]);
+  const [addBookView, setAddBookView] = useState(false);
   return (
-    <View >
-      
+    <View style={styles.container}>
+      <Text style={styles.bookText}>Lista de libros</Text>
+      <View style={styles.nav}>
+        <Navigation />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  
-});
+  container: {
+    justifyContent: 'flex-start',
+    backgroundColor: 'orange',
+    height: '100%'
+  },
+  bookText: {
+    textAlign: 'center',
+    marginTop: 50,
+    color: 'white',
+    fontSize: 16,
+  },
+  nav: {
+    justifyContent: 'flex-end',
+    flex: 1
+  }
+
+})
